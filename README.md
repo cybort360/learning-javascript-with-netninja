1930e423eeaa320d11750010174088df6db125e2
cybort360 <cybort360@gmail.com>
2024-02-02 04:24:53 +0100

    Section 20

--- README.md
+++
@@ -1,1 +0,0 @@
-learning javascript with netninja
---
+++ Section 20 - UI Library/src/index.js
@@ -0,0 +1,28 @@
+import Tooltip from './ui-library/tooltip';
+import Dropdown from './ui-library/dropdown';
+import Tabs from './ui-library/tabs';
+import Snackbar from './ui-library/snackbar';
+
+//create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+//create dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+})
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+//create snackbar
+const snackbar = new Snackbar();
+snackbar.init()
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)')
+})
